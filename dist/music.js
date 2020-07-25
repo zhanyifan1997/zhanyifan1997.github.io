36c331f2f50cad4308293c3781521017a144d973
ifan <380007101@q.com>
2020-07-25 13:47:27 +0800

    Site updated: 2020-07-25 13:47:23

---
+++ dist/music.js
@@ -0,0 +1,55 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: false,
+    autoplay: false,
+    audio: [
+      {
+        name: "像我这样的人",
+        artist: '毛不易',
+        url: 'dist/像我这样的人.mp3',
+        cover: 'dist/像我这样的人.jpg',
+      },
+      {
+        name: "伪装",
+        artist: '大壮',
+        url: 'dist/伪装.mp3',
+        cover: 'dist/伪装.jpg',
+      },
+      {
+        name: "消愁",
+        artist: '毛不易',
+        url: 'dist/消愁.mp3',
+        cover: 'dist/消愁.jpg',
+      },
+      {
+        name: "年少有为",
+        artist: '李荣浩',
+        url: 'dist/年少有为.mp3',
+        cover: 'dist/年少有为.jpg',
+      },
+      {
+        name: "勾指起誓",
+        artist: '洛天依,ilem',
+        url: 'dist/勾指起誓.mp3',
+        cover: 'dist/勾指起誓.jpg',
+      },
+      {
+        name: "叹云兮",
+        artist: '鞠婧祎',
+        url: 'dist/叹云兮.mp3',
+        cover: 'dist/叹云兮.jpg',
+      },
+      {
+        name: "落花成泥",
+        artist: '鞠婧祎',
+        url: 'dist/落花成泥.mp3',
+        cover: 'dist/落花成泥.jpg',
+      },
+      {
+        name: "女儿国",
+        artist: '张靓颖',
+        url: 'dist/女儿国.mp3',
+        cover: 'dist/女儿国.jpg',
+      }
+    ]
+});
